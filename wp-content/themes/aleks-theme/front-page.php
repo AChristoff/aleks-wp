1b871137ff5714661f3b2e47a692d82dc51095a5
AChristoff <aleksov101@gmail.com>
2020-04-19 21:21:19 +0300

    add products section

--- wp-content/themes/aleks-theme/front-page.php
+++ wp-content/themes/aleks-theme/front-page.php
@@ -21,18 +21,52 @@
             </section>
 
             <section class="about">
+                <h3>About us</h3>
                 <div class="content">
-                    <h3>About us</h3>
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                 </div>
-                <div class="images">
-                    <img alt="face_one" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_one.png">
-                    <img alt="face_two" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_one.png">
-                    <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_one.png">
+                <div class="about-images">
+                    <div class="avatar-wrapper">
+                        <img alt="face_one" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_one.png">
+                    </div>
+                    <div class="avatar-wrapper">
+                        <img alt="face_two" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_two.png">
+                    </div>
+                    <div class="avatar-wrapper">
+                        <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/face_three.png">
+                    </div>
                 </div>
             </section>
 
-            <section class="products"></section>
+            <section class="products">
+                <h3>Our products</h3>
+                <div class="content">
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                </div>
+                <div class="product-images">
+                    <div class="product-wrapper">
+                        <img alt="face_one" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_1.png">
+                    </div>
+                    <div class="product-wrapper">
+                        <img alt="face_two" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_2.png">
+                    </div>
+                    <div class="product-wrapper">
+                        <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_3.png">
+                    </div>
+                    <div class="product-wrapper">
+                        <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_4.png">
+                    </div>
+                    <div class="product-wrapper">
+                        <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_5.png">
+                    </div>
+                    <div class="product-wrapper">
+                        <img alt="face_three" src="<?php echo get_template_directory_uri(); ?>/assets/img/products/product_6.png">
+                    </div>
+                </div>
+            </section>
 
             <section class="services"></section>
 
